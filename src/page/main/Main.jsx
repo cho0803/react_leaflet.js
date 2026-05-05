5e09e95795151aed39d75a6a655228bf985a5815
cho0803 <cho0807s@naver.com>
2026-05-05 23:49:27 +0900

    베포용으로 수정

--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -2,8 +2,9 @@ export default function Main() {
   return <Portfolio />;
 }
 import Maps from "../maps/Maps.jsx";
-
 import Portfolio from "../projects/Portfolio.jsx";
+
+import Portfolio_bak from "../projects/Portfolio_bak.jsx";
 
 import Map from "../../app/components/maps/fragment/Leaflet.jsx";
 
