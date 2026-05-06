8fa75960dd68b1fc13ed86ccdd0fc7967352d459
cho0803 <cho0807s@naver.com>
2026-05-06 17:09:08 +0900

    공통 js에 useTheme, useMediaQuery 추가

--- src/app/components/maps/index.js
+++ src/app/components/maps/index.js
@@ -7,7 +7,7 @@ export { v4 as uuidv4 } from "uuid";
 export {styled} from 'styled-components';
 export {Box, Button, Menu, MenuItem  } from '@mui/material';
 
-export { Modal, Paper ,Divider, } from '@mui/material';
+export { Modal, Paper ,Divider, useTheme, useMediaQuery} from '@mui/material';
 export {AppBar,Toolbar, IconButton, InputBase, Typography, List, } from '@mui/material';
 
 // 속도 저하로 import 방식 변경 이유 확인필요
